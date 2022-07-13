e4973a20033053871fa9db3e1ed3ab4da2d71d1c
zisuu <david.ziswiler@finecloud.ch>
2022-07-13 06:43:05 +0200

    add regex exercise

---
+++ src/section4_regex/RegexPractice.java
@@ -0,0 +1,14 @@
+package section4_regex;
+
+public class RegexPractice {
+    public static void main(String[] args) {
+        System.out.println("car".matches("car")); // true
+        System.out.println("Car".matches("[Cc]ar")); // true
+        System.out.println("car".matches("[A-Ca-c]ar")); // true
+        System.out.println("jar".matches("\\war")); // true
+        System.out.println("5D_".matches("\\w\\w\\w")); // true
+        System.out.println("523".matches("\\d\\d\\d")); // true
+        System.out.println("523-231-4444".matches("\\d\\d\\d-\\d\\d\\d-\\d\\d\\d\\d")); // true
+        System.out.println("523-231-4444".matches("\\d{3}-\\d{3}-\\d{4}")); // true
+    }
+}
