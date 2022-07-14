1281907dce1fc187f8fa9340c003aba9521a4724
zisuu <david.ziswiler@finecloud.ch>
2022-07-14 21:59:37 +0200

    add some more regex

--- src/section4_regex/RegexPractice.java
+++ src/section4_regex/RegexPractice.java
@@ -1,5 +1,8 @@
 package section4_regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexPractice {
     public static void main(String[] args) {
         System.out.println("car".matches("car")); // true
@@ -17,5 +20,27 @@ public class RegexPractice {
         System.out.println("523231-4444".matches("\\d{3}[-.\\s]?\\d{3}[-.\\s]?\\d{4}")); // true
         System.out.println("523231-444".matches("\\d{3}[-.\\s]?\\d{3}[-.\\s]?\\d{3,4}")); // true
         System.out.println("523231-423423444".matches("\\d{3}[-.\\s]?\\d{3}[-.\\s]?\\d{3,}")); // true
+        System.out.println("523 231 4234".matches("(\\d{3}[-.\\s]?){2}\\d{3,}")); // true
+
+
+        String regex = """
+        # this is my regex to parse the parts of a phone number
+        (?:(?<countryCode>\\d{1,2})[-.\\s])? # gets countryCode
+        (?:\\(?(?<areaCode>\\d{3})\\)?[-.\\s]?) # gets areaCode
+        (?:(?<exchangeCode>\\d{3})[-.\\s]?) # gets exchangeCode
+        (?<lineNumber>\\d{4}) # gets lineNumber
+        """;
+        String phoneNumber = "(523) 231-4234";
+
+        Pattern pat = Pattern.compile(regex, Pattern.COMMENTS); // careful: Pattern.COMMENTS ignores whitespaces
+        Matcher mat = pat.matcher(phoneNumber);
+
+        if (mat.matches()){
+            //System.out.format("entire String: %s\n", mat.group(0));
+            System.out.format("Country Code: %s\n", mat.group("countryCode"));
+            System.out.format("Area Code: %s\n", mat.group("areaCode"));
+            System.out.format("Exchange Code: %s\n", mat.group("exchangeCode"));
+            System.out.format("Line number Code: %s\n", mat.group("lineNumber"));
+        }
     }
 }
--- src/section4_regex/TranscriptParser.java
+++ src/section4_regex/TranscriptParser.java
@@ -14,11 +14,26 @@ public class TranscriptParser {
                 Cumulative GPA (Unweighted)	3.46
                 """;
 
-        String regex = "Student\\sNumber:\\s(?<studentNum>\\d{10}).*";
-        Pattern pat = Pattern.compile(regex, Pattern.DOTALL);
+        String regex = """
+            Student\\sNumber:\\s(?<studentNum>\\d{10}).*
+            Grade:\\s+(?<grade>\\d{1,2}).*
+            Birthdate:\\s+(?<month>\\d{1,2})/(?<day>\\d{1,2})/(?<year>\\d{4}).*
+            Gender:\\s+(?<gender>\\w+\\b).*
+            State\\sID:\\s+(?<stateID>\\d+)\\b.*?
+            Cumulative.*?(?<weightedGPA>[\\d\\.]+)\\b.* # option1
+            # option2 Weighted\\)\\s+(?<weightedGPA>[\\d\\.]+)\\b.*
+            Unweighted\\)\\s+(?<unweightedGPA>[\\d\\.]+)\\b.*
+            """;
+        Pattern pat = Pattern.compile(regex, Pattern.DOTALL | Pattern.COMMENTS);
         Matcher mat = pat.matcher(transcript);
         if (mat.matches()) {
             System.out.println(mat.group("studentNum"));
+            System.out.println(mat.group("grade"));
+            System.out.println(mat.group("month") + "." + mat.group("day") + "." + mat.group("year"));
+            System.out.println(mat.group("gender"));
+            System.out.println(mat.group("stateID"));
+            System.out.println(mat.group("weightedGPA"));
+            //System.out.println(mat.group("unweightedGPA"));
         }
     }
 }
