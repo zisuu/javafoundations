949834b87e91299d045d233c5b337e1197f7e02d
zisuu <david.ziswiler@finecloud.ch>
2022-07-22 21:51:59 +0200

    add new testing stuff

--- src/main/section7_testing_code/GuessingGame.java
+++ src/main/section7_testing_code/GuessingGame.java
@@ -1,7 +1,24 @@
 package section7_testing_code;
 
+import java.util.Random;
+
 public class GuessingGame {
+
+    private final int randomNumber = new Random().nextInt(10) +1;
+    private int counter = 0;
+
     public String guess(int guessedInt) {
-        return "You got it";
+        counter++;
+        if (counter == 4 && guessedInt != getRandomNumber()) {
+            return "You didn't get it and you've had four tries. Game over.";
+        }
+
+        String tryText = counter == 1 ? "try" : "tries";
+        String winningMsg = String.format("You got it in %d %s", counter, tryText);
+        return guessedInt == getRandomNumber() ? winningMsg : "You didn't get it";
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
     }
 }
--- src/test/section7_testing_code/GuessingGameTest.java
+++ src/test/section7_testing_code/GuessingGameTest.java
@@ -2,15 +2,84 @@ package section7_testing_code;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GuessingGameTest {
 
+    public static final int GAME_RANDOM_RETRIES = 100;
+    private GuessingGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new GuessingGame();
+    }
+
     @Test
     public void testSimpleWinSituation(){
-        GuessingGame game = new GuessingGame();
-        String message = game.guess(3);
-        assertEquals("You got it", message);
+        int randomNum = game.getRandomNumber();
+        String message = game.guess(randomNum);
+        assertEquals("You got it in 1 try", message);
     }
+
+    @Test
+    public void testOneWrongNegGuessSituation(){
+        String message = game.guess(-5);
+        assertEquals("You didn't get it", message);
+    }
+
+    @Test
+    public void testOneWrongPosGuessSituation(){
+        int randomNum = game.getRandomNumber();
+        String message = game.guess(randomNum +1);
+        assertEquals("You didn't get it", message);
+    }
+
+    @RepeatedTest(10)
+    public void testRandomNumberGeneration(){
+        // 1 2 3 4 5 6 7 8 9 10
+        // 1 1 1 1 0 1 0 1 1 1 = 10
+        int[] rndNumCount = new int [11];
+        for (int counter = 0; counter < GAME_RANDOM_RETRIES; counter++){
+            GuessingGame game = new GuessingGame();
+            int randomNum = game.getRandomNumber();
+            rndNumCount[randomNum] = 1;
+        }
+
+        int sum = 0;
+        for (int counter = 0; counter < 11; counter++) {
+            sum += rndNumCount[counter];
+        }
+        assertEquals(10, sum);
+    }
+
+    @Test
+    public void testFourWrongGuesses(){
+        game.guess(-3);
+        game.guess(-3);
+        game.guess(-3);
+        String message = game.guess(-3);
+        assertEquals("You didn't get it and you've had four tries. Game over.", message);
+    }
+
+    @Test
+    public void testThreeWrongGuessesAndOneCorrect(){
+        game.guess(-3);
+        game.guess(-3);
+        game.guess(-3);
+        int  correctAnswer = game.getRandomNumber();
+        String message = game.guess(correctAnswer);
+        assertEquals("You got it in 4 tries", message);
+    }
+
+    @Test
+    public void testTwoWrongGuessesAndOneCorrect(){
+        game.guess(-3);
+        game.guess(-3);
+        int  correctAnswer = game.getRandomNumber();
+        String message = game.guess(correctAnswer);
+        assertEquals("You got it in 3 tries", message);
+    }
+
 }
