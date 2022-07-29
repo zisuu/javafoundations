62726ef37817e588d6ce140fa3d943d0f02718e2
zisuu <david.ziswiler@finecloud.ch>
2022-07-29 06:27:47 +0200

    add more oop

---
+++ src/main/section8_more_oop/exercises/Days.java
@@ -0,0 +1,11 @@
+package section8_more_oop.exercises;
+
+public enum Days {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+}
---
+++ src/main/section8_more_oop/exercises/Ex1.java
@@ -0,0 +1,21 @@
+package section8_more_oop.exercises;
+
+/**
+ * 1. Use an Enum to model the days of the week and print them using a loop.
+ * 1.1 Print them with the first letter capitalized (without changing the Enum constants).
+ * 1.2 Alternate between printing the first letter capitalized or whatever letter is approximately
+ * in the middle of the word. For example: Sunday, monDay, Tuesday, wednEsday, etc.
+ * 1.3 Print 10 randomly-chosen days of the week
+ */
+public class Ex1 {
+
+
+    public static void main(String[] args) {
+        Days day1 = new Days.MONDAY);
+
+//        for (int i = 0; i < 6; i++){
+//            System.out.println(days.ordinal() + i);
+//        }
+
+    }
+}
