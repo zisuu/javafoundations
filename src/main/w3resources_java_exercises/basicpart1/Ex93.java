0476c35a908ef3d7a06c315c6c4f0dd4814021fd
zisuuu <d.ziswiler@unico.ch>
2022-08-25 17:13:46 +0200

    add some more exercise

--- src/main/w3resources_java_exercises/basicpart1/Ex93.java
+++ src/main/w3resources_java_exercises/basicpart1/Ex93.java
@@ -1,7 +1,5 @@
 package w3resources_java_exercises.basicpart1;
 
-import java.util.Arrays;
-
 /**
  * 93. Write a Java program to test if an array of integers contains an element 10 next to 10
  * or an element 20 next to 20, but not both.
