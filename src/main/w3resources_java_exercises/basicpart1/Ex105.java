b4a51f0a3c07434e22ec1e5c485fb5185dcebd42
zisuuu <d.ziswiler@unico.ch>
2022-09-02 12:58:58 +0200

    add some more exercise

--- src/main/w3resources_java_exercises/basicpart1/Ex105.java
+++ src/main/w3resources_java_exercises/basicpart1/Ex105.java
@@ -5,7 +5,21 @@ package w3resources_java_exercises.basicpart1;
  */
 public class Ex105 {
     public static void main(String[] args) {
-//        Integer[] myNumbers1 = {2,3,10,20,20,21,102,10,20,21,0};
-//        Integer[] myNumbers2 = {2,3,10,20,20,21,102,10,20,21,0};
+        Integer[] myNumbers1 = {3,3,8,21,0};
+        Integer[] myNumbers2 = {3,3,5,22,0};
+
+        int groupOfNumbers = 2;
+        int checkStartEqual = 1;
+        int checkEndEqual = 1;
+        for (int i = 0; i < groupOfNumbers; i++) {
+            checkStartEqual = myNumbers1[i].compareTo(myNumbers2[i]);
+            checkEndEqual = myNumbers1[myNumbers1.length-(i+1)].compareTo(myNumbers2[myNumbers1.length-(i+1)]);
+        }
+
+        if (checkStartEqual == 0 && checkEndEqual == 0) {
+            System.out.println(true);;
+        } else {
+            System.out.println(false);
+        }
     }
 }
